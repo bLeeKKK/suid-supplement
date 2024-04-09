fa92ef8a8bc44048da47d05459f98b9b86e73f8d
bin6.li <bin6.li@changhong.com>
2024-04-09 14:14:20 +0800

    feat: 添加窗口固定组件

---
+++ src/SAddix/index.jsx
@@ -0,0 +1,21 @@
+import { useInViewport } from 'ahooks';
+import { Affix } from 'antd';
+import React, { useState } from 'react';
+
+const SAddix = ({ children, scope, ...props }) => {
+  const [affix, setAffix] = useState(false);
+  const [inViewport] = useInViewport(scope);
+  const child =
+    typeof children === 'function' ? children({ affix, inViewport }) : children;
+  if (scope?.current && !inViewport && affix) {
+    return child;
+  }
+
+  return (
+    <Affix {...props} onChange={setAffix}>
+      {child}
+    </Affix>
+  );
+};
+
+export default SAddix;
