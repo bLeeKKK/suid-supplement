c608f7173bf92390948a26f72b7e8751a5cf56ba
bin6.li <bin6.li@changhong.com>
2024-04-09 15:11:24 +0800

    Updates

--- Upload/UploadExample1.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { Upload } from "./..";
-export default (function () {
-  return /*#__PURE__*/React.createElement(Upload, null);
-});
--- Upload/UploadExample3.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Upload } from "./..";
-export default (function () {
-  return /*#__PURE__*/React.createElement(Upload, {
-    pastePower: false,
-    dragPower: false
-  });
-});
