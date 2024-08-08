fd01a81e3d4ece1c7564b8b517de7a141c4314bf
bLeeKKK <bLeeKKK@users.noreply.github.com>
2024-08-08 05:20:28 +0000

    deploy: 07fedd04ee8a6f55fb85ff819ac3e03c5cd29208

---
+++ meta__SBtnBox.3bf5bc85.async.js
@@ -0,0 +1,51 @@
+"use strict";(self.webpackChunksuid_supplement=self.webpackChunksuid_supplement||[]).push([[367],{89249:function(l,e,n){n.r(e),n.d(e,{demos:function(){return p}});var o=n(17061),s=n.n(o),a=n(17156),d=n.n(a),u=n(67294),c=n(99464),p={"sbtnbox-demo-example2":{component:u.memo(u.lazy(function(){return n.e(855).then(n.bind(n,60256))})),asset:{type:"BLOCK",id:"sbtnbox-demo-example2",refAtomIds:["SBtnBox"],dependencies:{"index.jsx":{type:"FILE",value:n(75344).Z},react:{type:"NPM",value:"16.14.0"},"suid-supplement":{type:"NPM",value:"0.1.55"}},entry:"index.jsx"},context:{react:n(67294),"suid-supplement":n(92758)},renderOpts:{compile:function(){var m=d()(s()().mark(function x(){var r,_=arguments;return s()().wrap(function(t){for(;;)switch(t.prev=t.next){case 0:return t.next=2,Promise.all([n.e(764),n.e(335)]).then(n.bind(n,37335));case 2:return t.abrupt("return",(r=t.sent).default.apply(r,_));case 3:case"end":return t.stop()}},x)}));function i(){return m.apply(this,arguments)}return i}()}}}},38863:function(l,e,n){n.r(e),n.d(e,{texts:function(){return s}});var o=n(99464);const s=[{value:"\u4F7F\u7528 antd@3 \u7684 SModal \u7EC4\u4EF6\u5C01\u88C5\u7684\u4E0A\u4F20\u7EC4\u4EF6",paraId:0,tocIndex:0},{value:"\u5C5E\u6027",paraId:1},{value:"\u63CF\u8FF0",paraId:1},{value:"\u7C7B\u578B",paraId:1},{value:"\u9ED8\u8BA4\u503C",paraId:1},{value:"xxx",paraId:1},{value:"xxx",paraId:1},{value:"'file'",paraId:1},{value:"\u3001",paraId:1},{value:"'btn'",paraId:1},{value:"file",paraId:1}]},75344:function(l,e){e.Z=`import React from 'react';
+import { BtnsBox } from 'suid-supplement';
+
+export default () => {
+  return (
+    <BtnsBox
+      limt="3"
+      btnArr={[
+        {
+          title: '\u67E5\u770B',
+          tip: '\u67E5\u770Bxxxxxxxxx',
+          type: 'primary',
+          icon: 'file-search',
+          onClick: () => {
+            const p = new Promise((resolve) => {
+              setTimeout(resolve, 2000);
+            });
+            return p;
+          },
+        },
+        {
+          title: '\u7F16\u8F91',
+          icon: 'edit',
+          disabled: true,
+          onClick: console.log,
+        },
+        {
+          title: '\u5220\u9664',
+          icon: 'delete',
+          onClick: console.log,
+        },
+        {
+          title: '\u6253\u5370',
+          icon: 'printer',
+          onClick: console.log,
+          tip: 'xxxx xxxx xxxx xxxx z',
+          disabled: true,
+          onClick: (e) => {
+            console.log('printer');
+            const p = new Promise((resolve) => {
+              console.log(e);
+              setTimeout(resolve, 2000);
+            });
+            return p;
+          },
+        },
+      ]}
+    />
+  );
+};
+`}}]);
